a33507d4381b982403d30fe33ff0953421d9b29f
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-08-30 02:01:09 +0900

    テスト表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,47 @@
+import { ActionIcon, Button, useMantineColorScheme } from "@mantine/core";
 import {
-  ActionIcon,
-  AspectRatio,
-  Button,
-  useMantineColorScheme,
-} from "@mantine/core";
-import { useNFTDrop } from "@thirdweb-dev/react";
+  ChainId,
+  ConnectWallet,
+  ThirdwebNftMedia,
+  useAddress,
+  useChainId,
+  useMetamask,
+  useNetwork,
+  useNetworkMismatch,
+  useNFT,
+  useNFTCollection,
+  useNFTDrop,
+} from "@thirdweb-dev/react";
 import type { CustomNextPage, NextPage } from "next";
 import { useEffect } from "react";
 import { Layout } from "src/layouts";
 import { Moon, Sun } from "tabler-icons-react";
 
 const Home: CustomNextPage = () => {
+  // themeのセットアップ
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
   const dark = colorScheme === "dark";
 
-  // const nftDrop = useNFTDrop(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS);
-  useEffect(() => {
-    // const result = await contract.burnToken(tokenId);
-    // console.log(nftDrop);
-  }, []);
+  // 表示
+  const contract = useNFTCollection(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS);
+  const { data: nft, isLoading } = useNFT(contract, 0);
+  console.log(nft);
 
-  const handleClick = () => {
-    console.log("Hello!");
-  };
+  // ネットワークの検知と変更
+  const isMismatched = useNetworkMismatch();
+  const [, switchNetwork] = useNetwork();
+  const address = useAddress(); // Get connected wallet address
+  const connectWithMetamask = useMetamask(); // Connect wallet with Metamask
+  useEffect(() => {
+    if (isMismatched) {
+      switchNetwork(ChainId.Mumbai);
+    }
+  }, [address]);
+
+  // ウォレットが接続されているネットワークのチェーン ID
+  const chainId = useChainId();
+  // 全体的なデータ
+  // const nftDrop = useNFTDrop(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS);
 
   return (
     <div className="p-20 ">
@@ -34,15 +53,33 @@ const Home: CustomNextPage = () => {
       >
         {dark ? <Moon size={18} /> : <Sun size={18} />}
       </ActionIcon>
-      <Button onClick={handleClick}>Lime green</Button>
-      <button className="bg-black p-2 text-white">ぼたん</button>
-      <AspectRatio ratio={16 / 9}>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.3063874233135!2d-74.04668908358428!3d40.68924937933441!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25090129c363d%3A0x40c6a5770d25022b!2sStatue%20of%20Liberty%20National%20Monument!5e0!3m2!1sen!2sru!4v1644262070010!5m2!1sen!2sru"
-          title="Google map"
-          frameBorder="0"
+      <div>
+        {/* ウォレット接続ボタン */}
+        <ConnectWallet
+          colorMode={dark ? "dark" : "light"}
+          accentColor="#F213A4"
         />
-      </AspectRatio>
+        {/* mintされる前のNFTを表示 */}
+        {!isLoading && nft ? (
+          <ThirdwebNftMedia metadata={nft.metadata} />
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+      <hr />
+      {/* ネットワークの検知 */}
+      {isMismatched && (
+        <button onClick={() => switchNetwork(ChainId.Mumbai)}>
+          Switch Network
+        </button>
+      )}
+      {address ? (
+        <h4>Connected as {address}</h4>
+      ) : (
+        <button onClick={connectWithMetamask}>Connect Metamask Wallet</button>
+      )}
+      <hr />
+      <div>{chainId}</div>
     </div>
   );
 };
