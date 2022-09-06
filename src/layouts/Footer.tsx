6edac7f04d4e7a4e3e48055031b3e0bd5059bebb
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-09-06 23:18:19 +0900

    💄

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -6,23 +6,10 @@ export const Footer: FC = () => {
   return (
     <footer className="bg-violet-700 text-white">
       <div className="inner">
-        <div className="flex items-center justify-between py-5">
-          <div>
-            <Link href={pagesPath.$url()}>
-              <a>
-                <h2 className="text-3xl font-bold">Parallel Space test net</h2>
-              </a>
-            </Link>
-          </div>
-          <div className="flex items-center justify-between py-5">
-            <nav className="flex">
-              <Link href={pagesPath.$url()}>
-                <a>
-                  <h2 className="p-5 text-lg font-bold">Home</h2>
-                </a>
-              </Link>
-            </nav>
-          </div>
+        <div className="py-3 md:py-10 text-center">
+          <Link href={pagesPath.$url()}>
+            <a className="text-lg md:text-xl font-bold">Parallel Space test net</a>
+          </Link>
         </div>
       </div>
     </footer>
