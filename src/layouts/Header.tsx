66b9321f9027984a467464920288141d3685381d
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-09-06 23:21:15 +0900

    NavとDrawerをコンポーネント化してリファクタリングを行った。

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,12 +1,12 @@
 import {
   ActionIcon,
-  Burger,
   Button,
-  Drawer,
   useMantineColorScheme,
 } from "@mantine/core";
 import Link from "next/link";
-import { FC, useState } from "react";
+import { FC} from "react";
+import { DrawerMenu } from "src/components/elements/DrawerMenu";
+import { NavMenu } from "src/components/elements/NavMenu";
 import { useConnectWallet } from "src/hooks/useConnectWallet";
 import { pagesPath } from "src/utils/$path";
 import { Moon, Sun } from "tabler-icons-react";
@@ -26,11 +26,6 @@ export const Header: FC = () => {
     return excerptText;
   };
 
-  //memo drawerの開いているかを確認
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const toggleDrawer = () => {
-    setIsOpen((prevState) => !prevState);
-  };
   return (
     <header>
       <div className="inner">
@@ -38,32 +33,21 @@ export const Header: FC = () => {
           <div>
             <Link href={pagesPath.$url()}>
               <a>
-                <h2 className="text-3xl font-bold">Parallel Space</h2>
+                <h2 className=" text-lg font-bold md:text-3xl">
+                  Parallel Space
+                </h2>
               </a>
             </Link>
           </div>
           <div className="flex items-center justify-between py-5">
-            <div className="hidden sm:block">
-              <nav className="flex">
-                <Link href={pagesPath.collection.$url()}>
-                  <a>
-                    <h2 className="p-5 text-lg font-bold">collection</h2>
-                  </a>
-                </Link>
-                <Link href={pagesPath.owner.$url()}>
-                  <a>
-                    <h2 className="p-5 text-lg font-bold">owner</h2>
-                  </a>
-                </Link>
-              </nav>
-            </div>
+            <NavMenu/>
             {/* テーマの切り替え */}
             <ActionIcon
               variant="outline"
               color={dark ? "blue" : "yellow"}
               onClick={() => toggleColorScheme()}
               title="Toggle color scheme"
-              className="mr-1"
+              className="mr-4"
             >
               {dark ? <Moon size={18} /> : <Sun size={18} />}
             </ActionIcon>
@@ -79,31 +63,7 @@ export const Header: FC = () => {
               </Button>
             )}
           </div>
-          <div className="sm:hidden ">
-            <Burger onClick={toggleDrawer} opened={isOpen} size={30} />
-            <Drawer
-              opened={isOpen}
-              onClose={toggleDrawer}
-              position="right"
-              padding="xl"
-              size="61%"
-            >
-              <div>
-                <nav className="">
-                  <Link href={pagesPath.collection.$url()}>
-                    <a>
-                      <h2 className="p-5 text-lg font-bold">collection</h2>
-                    </a>
-                  </Link>
-                  <Link href={pagesPath.owner.$url()}>
-                    <a>
-                      <h2 className="p-5 text-lg font-bold">owner</h2>
-                    </a>
-                  </Link>
-                </nav>
-              </div>
-            </Drawer>
-          </div>
+          <DrawerMenu />
         </div>
       </div>
     </header>
