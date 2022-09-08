d1157e64e5b0b74341ac8dfed5caac09039b3ab3
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-09-09 00:45:57 +0900

    viewportが360px以下の場合、表示崩れしないようにviewportが360pxに固定されて表示されるようにした。

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,14 +3,25 @@ import Head from "next/head";
 import { usePageLoading } from "src/hooks/usePageLoading";
 import { Footer } from "src/layouts/Footer";
 import { Header } from "src/layouts/Header";
+import { useViewportSize } from "@mantine/hooks";
 
 export const Layout: CustomLayout = (props) => {
   const { pageLoading, loadingComponent } = usePageLoading();
+  const { width } = useViewportSize();
+
   return (
     <>
       <Head>
         <title>mumbai mint site</title>
-        <meta name="description" content="This site can mint NFTs in parallel space on polygon networks."></meta>
+        <meta
+          name="description"
+          content="This site can mint NFTs in parallel space on polygon networks."
+        ></meta>
+        {width > 360 ? (
+          <meta name="viewport" content="width=device-width" />
+        ) : (
+          <meta name="viewport" content="width=360" />
+        )}
       </Head>
       <div className="body">
         {pageLoading ? (
