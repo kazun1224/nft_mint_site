4b74e3650433444289e605a8c56596be936678d0
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-09-08 22:24:07 +0900

    propsの型定義の変更　NFTを表示するUIの変更

--- src/components/elements/nftListItem.tsx
+++ src/components/elements/nftListItem.tsx
@@ -1,21 +1,20 @@
-import { Image, Text } from "@mantine/core";
+import { Card, Image, Text } from "@mantine/core";
+import { NFTMetadataOwner } from "@thirdweb-dev/sdk";
 import { FC } from "react";
 
 export type NftListItemProps = {
-  token: any;
+  token: NFTMetadataOwner;
 };
 export const NftListItem: FC<NftListItemProps> = ({ token }) => {
-  return (
-    <div className="mx-auto w-full max-w-screen-lg px-5 md:px-20">
-      <Image
 
-        src={token.metadata.image}
-        alt="NFT"
-        withPlaceholder
-      />
-      <div>
-        <Text size="md">{token.metadata.name}</Text>
-      </div>
-    </div>
+  return (
+    <Card shadow="sm" p="lg" radius="md" withBorder>
+      <Card.Section>
+        <Image src={token.metadata.image || undefined} alt="NFT" withPlaceholder />
+      </Card.Section>
+      <Text weight={500} size="lg" className="mt-5">
+        {token.metadata.name}
+      </Text>
+    </Card>
   );
 };
